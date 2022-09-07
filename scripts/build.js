eb6465db809bbd8e87c3dcdb371209d1e7ca35db
Le Vivilet <levivlet@pm.me>
2022-09-07 18:56:17 +0200

    try to fix build

--- scripts/build.js
+++ scripts/build.js
@@ -1,5 +1,5 @@
 import { execSync } from 'child_process'
-import fs, { readFileSync } from 'fs'
+import fs, { mkdirSync, readFileSync } from 'fs'
 import path, { dirname, join } from 'path'
 import { fileURLToPath } from 'url'
 import { packageExtension } from '@lvce-editor/package-extension'
@@ -10,6 +10,8 @@ const __dirname = dirname(fileURLToPath(import.meta.url))
 
 const root = path.join(__dirname, '..')
 const extension = path.join(root, 'packages', 'extension')
+const languageService = path.join(root, 'packages', 'language-service')
+const languageServiceData = path.join(root, 'packages', 'language-service-data')
 
 fs.rmSync(join(root, 'dist'), { recursive: true, force: true })
 
@@ -22,6 +24,8 @@ delete packageJson.xo
 delete packageJson.jest
 delete packageJson.prettier
 delete packageJson.devDependencies
+delete packageJson.scripts
+packageJson.dependencies['language-service'] = '^0.0.0-dev'
 
 fs.writeFileSync(
   join(root, 'dist', 'package.json'),
@@ -36,9 +40,9 @@ fs.copyFileSync(
 fs.cpSync(join(extension, 'src'), join(root, 'dist', 'src'), {
   recursive: true,
 })
-fs.cpSync(join(extension, 'data'), join(root, 'dist', 'data'), {
-  recursive: true,
-})
+// fs.cpSync(join(extension, 'data'), join(root, 'dist', 'data'), {
+//   recursive: true,
+// })
 
 const getAllDependencies = (obj) => {
   if (!obj || !obj.dependencies) {
@@ -47,24 +51,35 @@ const getAllDependencies = (obj) => {
   return [obj, ...Object.values(obj.dependencies).flatMap(getAllDependencies)]
 }
 
-const getDependencies = () => {
+const getDependencies = (folder) => {
   const stdout = execSync('npm list --omit=dev --parseable --all', {
-    cwd: extension,
+    cwd: folder,
   }).toString()
   const lines = stdout.split('\n')
   return lines.slice(1, -1)
 }
 
-const dependencies = getDependencies()
-for (const dependency of dependencies) {
-  fs.cpSync(
-    dependency,
-    join(root, 'dist', dependency.slice(extension.length)),
-    {
+for (const folder of [languageService]) {
+  const dependencies = getDependencies(folder)
+  for (const dependency of dependencies) {
+    fs.cpSync(dependency, join(root, 'dist', dependency.slice(folder.length)), {
       recursive: true,
-    }
-  )
+    })
+  }
 }
+
+mkdirSync(join(root, 'dist', 'node_modules', 'language-service'), {
+  recursive: true,
+})
+fs.cpSync(
+  join(root, 'packages', 'language-service', 'src'),
+  join(root, 'dist', 'node_modules', 'language-service', 'src'),
+  { recursive: true }
+)
+fs.copyFileSync(
+  join(root, 'packages', 'language-service', 'package.json'),
+  join(root, 'dist', 'node_modules', 'language-service', 'package.json')
+)
 
 for (const notNeeded of NOT_NEEDED) {
   fs.rmSync(join(root, 'dist', notNeeded), { force: true, recursive: true })
