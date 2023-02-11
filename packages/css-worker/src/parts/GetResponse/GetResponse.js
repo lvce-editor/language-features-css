98a2f46a8c038ec9ba1d9da56336b54547957f5a
Le Vivilet <levivlet@pm.me>
2023-02-11 14:01:47 +0100

    fix tab completion

--- packages/css-worker/src/parts/GetResponse/GetResponse.js
+++ packages/css-worker/src/parts/GetResponse/GetResponse.js
@@ -4,16 +4,14 @@ import * as TabCompletion from '../TabCompletion/TabCompletion.js'
 
 export const getResponse = async (method, params) => {
   if (method === CssWorkerCommandType.GetTabCompletion) {
-    const uri = params[0]
-    const content = params[1]
-    const offset = params[2]
+    const content = params[0]
+    const offset = params[1]
     const result = await TabCompletion.cssTabCompletion(content, offset)
     return result
   }
   if (method === CssWorkerCommandType.GetCompletion) {
-    const uri = params[0]
-    const content = params[1]
-    const offset = params[2]
+    const content = params[0]
+    const offset = params[1]
     const result = Completion.cssCompletion(content, offset)
     return result
   }
