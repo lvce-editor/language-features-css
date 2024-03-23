ddbe4eccac28e8c5e057ba86aea87ed4f5bfbac7
Le Vivilet <levivlet@pm.me>
2024-03-23 12:00:28 +0100

    feature: simplify static build

--- packages/build/src/build-static.js
+++ packages/build/src/build-static.js
@@ -1,7 +1,14 @@
 import { exportStatic } from '@lvce-editor/shared-process'
-import { cp, readdir, readFile, writeFile } from 'node:fs/promises'
+import { cp, readdir } from 'node:fs/promises'
 import path from 'node:path'
 import { root } from './root.js'
+
+await import('./build.js')
+
+await cp(path.join(root, 'dist'), path.join(root, 'dist2'), {
+  recursive: true,
+  force: true,
+})
 
 await exportStatic({
   extensionPath: 'packages/extension',
@@ -17,36 +24,8 @@ const isCommitHash = (dirent) => {
 const dirents = await readdir(path.join(root, 'dist'))
 const commitHash = dirents.find(isCommitHash) || ''
 
-for (const dirent of ['src', 'data']) {
-  await cp(
-    path.join(root, 'packages', 'css-worker', dirent),
-    path.join(
-      root,
-      'dist',
-      commitHash,
-      'extensions',
-      'builtin.language-features-css',
-      'css-worker',
-      dirent
-    ),
-    { recursive: true, force: true }
-  )
-}
-
-const workerUrlFilePath = path.join(
-  root,
-  'dist',
-  commitHash,
-  'extensions',
-  'builtin.language-features-css',
-  'src',
-  'parts',
-  'CssWorkerUrl',
-  'CssWorkerUrl.js'
+await cp(
+  path.join(root, 'dist2'),
+  path.join(root, 'dist', commitHash, 'extensions', 'builtin.prettier'),
+  { recursive: true, force: true }
 )
-const oldContent = await readFile(workerUrlFilePath, 'utf8')
-const newContent = oldContent.replace(
-  '../../../../css-worker/src/cssWorkerMain.js',
-  '../../../css-worker/src/cssWorkerMain.js'
-)
-await writeFile(workerUrlFilePath, newContent)
