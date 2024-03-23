59ca2288898777e74607b96fdffee9f1312d5201
Le Vivilet <levivlet@pm.me>
2024-03-23 13:13:17 +0100

    fix: json import

--- packages/build/src/replace.js
+++ packages/build/src/replace.js
@@ -2,6 +2,6 @@ import { readFileSync, writeFileSync } from 'node:fs'
 
 export const replace = ({ path, occurrence, replacement }) => {
   const oldContent = readFileSync(path, 'utf8')
-  const newContent = oldContent.replace(occurrence, replacement)
+  const newContent = oldContent.replaceAll(occurrence, replacement)
   writeFileSync(path, newContent)
 }
