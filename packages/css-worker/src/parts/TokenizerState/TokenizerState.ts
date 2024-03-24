5af5ff816a3ad280c2df3bcfbb53663f1f7a8df2
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-03-24 19:12:26 +0100

    feature: move tokenizer state enum into separate file (#153)

---
+++ packages/css-worker/src/parts/TokenizerState/TokenizerState.ts
@@ -0,0 +1,6 @@
+export const TopLevelContent = 1
+export const AfterSelector = 2
+export const InsideSelector = 3
+export const AfterPropertyName = 4
+export const AfterPropertyNameAfterColon = 5
+export const AfterPropertyValue = 6
