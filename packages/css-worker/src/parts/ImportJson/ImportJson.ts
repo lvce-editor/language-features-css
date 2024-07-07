a4971d70dcebf54c1164cdb8fa20d7064f174f3e
Le Vivilet <levivlet@pm.me>
2024-07-07 13:59:10 +0200

    fix error

--- packages/css-worker/src/parts/ImportJson/ImportJson.ts
+++ packages/css-worker/src/parts/ImportJson/ImportJson.ts
@@ -1,14 +1,14 @@
 const importJsonNode = async (path) => {
   const fs = await import('fs/promises')
   const { fileURLToPath } = await import('url')
-  const absoluteUri = new URL(`../../../${path}`, import.meta.url).toString()
+  const absoluteUri = new URL(`../../../../${path}`, import.meta.url).toString()
   const absolutePath = fileURLToPath(absoluteUri)
   const data = await fs.readFile(absolutePath, 'utf8')
   return JSON.parse(data)
 }
 
 const importJsonBrowser = async (path) => {
-  const absolutePath = import.meta.resolve(`../../../${path}`)
+  const absolutePath = import.meta.resolve(`../../../../${path}`)
   const response = await fetch(absolutePath)
   if (!response.ok) {
     throw new Error(response.statusText)
