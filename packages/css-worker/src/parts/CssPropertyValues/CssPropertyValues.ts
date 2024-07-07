3617166e3d3e6cc3a7858f75521052517b935d25
Le Vivilet <levivlet@pm.me>
2024-07-07 13:58:44 +0200

    feature: move css data into separate folder

--- packages/css-worker/src/parts/CssPropertyValues/CssPropertyValues.ts
+++ packages/css-worker/src/parts/CssPropertyValues/CssPropertyValues.ts
@@ -2,5 +2,5 @@ import * as ImportJson from '../ImportJson/ImportJson.ts'
 
 // TODO use import json once supported
 export const cssPropertyValues = await ImportJson.importJson(
-  'data/css-property-values.json'
+  'css-data/src/css-property-values.json'
 )
