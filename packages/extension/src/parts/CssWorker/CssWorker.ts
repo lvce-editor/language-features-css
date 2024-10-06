121c39b2b06a282eaff19d413d0a4c633680fbf8
Le Vivilet <levivlet@pm.me>
2024-10-06 15:00:30 +0200

    feature: simplify rpc

--- packages/extension/src/parts/CssWorker/CssWorker.ts
+++ packages/extension/src/parts/CssWorker/CssWorker.ts
@@ -1,27 +1,8 @@
-import * as CssWorkerUrl from '../CssWorkerUrl/CssWorkerUrl.ts'
+// @ts-ignore
+const rpc = vscode.createRpc({
+  id: 'builtin.language-features-css.css-worker',
+})
 
-export const state: any = {
-  rpcPromise: undefined,
-}
-
-const createRpc = async () => {
-  // @ts-ignore
-  const rpc = await vscode.createRpc({
-    type: 'worker',
-    url: CssWorkerUrl.cssWorkerUrl,
-    name: 'Css Worker',
-  })
-  return rpc
-}
-
-const getOrCreateRpc = async () => {
-  if (!state.rpcPromise) {
-    state.rpcPromise = createRpc()
-  }
-  return state.rpcPromise
-}
-
-export const getInstance = async () => {
-  const rpc = await getOrCreateRpc()
-  return rpc
+export const invoke = (method, ...params) => {
+  return rpc.invoke(method, ...params)
 }
